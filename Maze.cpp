9093c3711faac15cd82f5ff1f29bf1c1d9800a8b
Nathan Smith <nathansmith@localhost.localdomain>
2018-09-24 11:45:10 -0400

    Adding comments to Maze.cpp

--- Maze.cpp
+++ Maze.cpp
@@ -10,21 +10,28 @@
 #define NODES 25
 #define STROKE 5
 
+/*This function returns the next vertex of the graph given by depth first
+  search with a recursive backtracker*/
 int recursiveBacktracker(MatrixGraph, MatrixGraph*, std::vector<int>*, std::unordered_set<int>*, int);
 
 int main(int argc, char **argv) {
+    //Shapes
     std::vector<sf::RectangleShape> rects;
     std::vector<sf::RectangleShape> bottomEdges;
     std::vector<sf::RectangleShape> rightEdges;
+    //The top and left borders are fixed
     sf::RectangleShape topBorder;
     sf::RectangleShape leftBorder;
+    sf::RenderWindow window;
+
+    //Model variables
     MatrixGraph grid, maze;
     std::vector<int> stack;
     std::unordered_set<int> visitedSet;
     int i, currentVertex, row, column, j, seed;
-    sf::RenderWindow window;
     char *commandLine;
 
+    //Get command line arg, seed random
     commandLine = *(argv+1);
     seed = atoi(commandLine);
     srand(seed);
@@ -59,16 +66,20 @@ int main(int argc, char **argv) {
         }
     }
 
+    //Start at the vertex 0
     currentVertex = 0;
     visitedSet.insert(currentVertex);
     stack.push_back(currentVertex);
 
+    //Create the SFML window, set framerate
     window.create(sf::VideoMode(WINDOW_WIDTH, WINDOW_HEIGHT), "Maze");
-    window.setFramerateLimit(60);
+    window.setFramerateLimit(15);
 
-    //Set up shapes
+    //Set up borders
     topBorder = sf::RectangleShape(sf::Vector2f(WINDOW_WIDTH, STROKE));
     leftBorder = sf::RectangleShape(sf::Vector2f(STROKE, WINDOW_HEIGHT));
+
+    //Set up vertices
     row = 0;
     column = 0;
     for( i = 0; i < NODES * NODES; i++ ) {
@@ -85,6 +96,7 @@ int main(int argc, char **argv) {
         }
     }
 
+    //Set up edges
     row = 0;
     column = 0;
     for( i = 0; i < NODES * NODES; i++ ) {
@@ -100,7 +112,6 @@ int main(int argc, char **argv) {
             row++;
         }
     }
-
     row = 0;
     column = 0;
     for( i = 0; i < NODES * NODES; i++ ) {
@@ -116,20 +127,26 @@ int main(int argc, char **argv) {
             row++;
         }
     }
+    for( i = 0; i < rects.size(); i++) {
+        rects.at(i).setFillColor(sf::Color::White);
+        bottomEdges.at(i).setFillColor(sf::Color::Cyan);
+        rightEdges.at(i).setFillColor(sf::Color::Cyan);
+    }
 
+    //Main loop
     while( window.isOpen() ) {
+        //Handle events in order to close the window
         sf::Event event;
         while( window.pollEvent(event) ){
             if(event.type == sf::Event::Closed) {
                 window.close();
             }
         }
+
+        //Begin frame
         window.clear(sf::Color::Black);
-        for( i = 0; i < rects.size(); i++) {
-            rects.at(i).setFillColor(sf::Color::White);
-            bottomEdges.at(i).setFillColor(sf::Color::Cyan);
-            rightEdges.at(i).setFillColor(sf::Color::Cyan);
-        }
+
+        //Indicate current position
         rects.at(currentVertex).setFillColor(sf::Color::Red);
 
         /* Drawing Logic */
@@ -140,6 +157,7 @@ int main(int argc, char **argv) {
             window.draw(rects.at(i));
         }
         
+        //Draw existent edges
         for( i = 0; i < rightEdges.size(); i++ ) {
             std::vector<int> neighbors = maze.get_neighbors(i);
             for( j = 0; j < neighbors.size(); j++ ) {
@@ -158,6 +176,9 @@ int main(int argc, char **argv) {
             }
         }
         window.display();
+        //End frame
+
+        //Get next position from recursive backtracker
         if(!stack.empty()) {
             currentVertex = recursiveBacktracker(grid, &maze, &stack, &visitedSet, currentVertex);
         }
@@ -173,16 +194,21 @@ std::unordered_set<int> *visitedSet, int currentVertex) {
     std::vector<int> neighbors;
     int isvisited;
 
+    //From the neighbors of the current node, determine which are unvisited
     neighbors = grid.get_neighbors(currentVertex);
     for( i = 0; i < neighbors.size(); i++ ) {
         if(!(*visitedSet).count(neighbors.at(i))) {
             unvisitedNeighbors.push_back(neighbors.at(i));
         }
     }
+
     if(!unvisitedNeighbors.empty()) {
+        //Get a random neighbor
         std::random_shuffle (unvisitedNeighbors.begin(), unvisitedNeighbors.end());
         nextVertex = unvisitedNeighbors.back();
+        //Push current node onto the stack
         (*stack).push_back(currentVertex);
+        //Add the appropriate edge to the maze
         (*maze).add_edge(currentVertex, nextVertex);
         (*visitedSet).insert(nextVertex);
     } else if (!(*stack).empty()) {
